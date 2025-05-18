252a74ccddf2f7a09f002d946a250b9da3675813
Eduarda <eotobelli@gmail.com>
2025-05-18 16:39:11 -0300

    templete13

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
-
 def post_list(request): 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {})
@@ -15,5 +10,19 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
+
+def post_new(request):
+form = PostForm()
+return render(request, 'blog/post_edit.html', {'form': form})
+
+if request.method == "POST":
+    form = PostForm(request.POST)
+    if form.is_valid():
+     post = form.save(commit=False)
+     post.author = request.user
+     post.published_date = timezone.now()
+     post.save()
+else:
+    form = PostForm()
+
 from django.shortcuts import render, get_object_or_404
